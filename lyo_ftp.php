5a4bcf8ef91e67f7645c37743f05b406deb495b8
oxdead <aerahproject@gmail.com>
2020-12-09 17:18:38 +0200

    redo class

--- lyo_ftp.php
+++ lyo_ftp.php
@@ -1,26 +1,73 @@
 <?php
 namespace Lyo;
-
 require "lyo_funcs_general.php";
 use function \Lyo\Funcs\General\{extractFilepath, extractFilename, isValidDir, isStrEndsWith};
 
+
+
+//todo: use ob
+//https://www.php.net/manual/en/book.outcontrol.php
+//https://stackoverflow.com/questions/927341/upload-entire-directory-via-php-ftp
+
+//make custom exception interface (inherit from Exception ??) and assign to ftphandler
+
+
 class FtpHandler
 {
+    private $hConnection = null;
+    private $blackList = array('.', '..', 'Thumbs.db');
+
+    public function __construct($ftpHost = "", $ftpUser = "", $ftpPass = "") 
+    {
+        if ($ftpHost != "") 
+        {
+            $this->hConnection = \ftp_connect($ftpHost);
+            if (isset($this->hConnection)) 
+            {
+                if (\ftp_login($this->hConnection, $ftpUser, $ftpPass)) 
+                {
+                    //nlist, rawlist, get, put do not work in active mode
+                    \ftp_pasv($this->hConnection, true);
+                } 
+                else 
+                {
+                    ftp_close($this->hConnection);
+                    unset($this->hConnection);
+                    //todo: handle Exception properly
+                    echo("Error: ftp: $ftpUser: Login name or password is incorrect!");
+                }
+            }
+            else
+            {
+                //todo: handle Exception properly
+                echo ("Error: ftp: $ftpHost: Connection has failed!");
+            }
+        }
+    }
+
+    public function __destruct() 
+    {
+        if (isset($this->hConnection)) 
+        {
+            ftp_close($this->hConnection);
+            unset($this->hConnection);
+        }
+    }
+
+
     // works for folders and files, doesn't work for root folder '/'
-    public function isFileExist($ftpConn, $fullPath)
+    public function isFileExist($fullPath)
     {
         $fullPath = \rtrim($fullPath, "/\\");
         $parentPath = extractFilepath($fullPath);
         if(isset($parentPath) && \strlen($parentPath) == 0) { $parentPath = '/'; }
         $fileName = extractFilename($fullPath);
 
-        
-
         if(isValidDir($parentPath))
         {
-            if (\ftp_chdir($ftpConn, $parentPath))
+            if (\ftp_chdir($this->hConnection, $parentPath))
             {
-                $files = \ftp_nlist($ftpConn, "-A .");
+                $files = \ftp_nlist($this->hConnection, "-A .");
 
                 foreach($files as $file)
                 {
@@ -34,19 +81,76 @@ class FtpHandler
         return false;
     }
 
+    public function makeDir($ftpFullPathDir) 
+    {
+        $error = "";
+        try 
+        {
+            \ftp_mkdir($this->hConnection, $ftpFullPathDir);
+        } 
+        catch (\Exception $e) 
+        {
+            if ($e->getCode() == 2) $error = $e->getMessage(); 
+        }
+        return $error;
+    }
+
     
-    
-    public function deleteDirAndFiles($ftpConn, $fullPathDir)
+    public function putFile($localPath, $remotePath) 
+    {
+        $error = "";
+        try 
+        {
+            \ftp_put($this->hConnection, $remotePath, $localPath, FTP_BINARY); 
+        } 
+        catch (\Exception $e) 
+        {
+            if ($e->getCode() == 2) $error = $e->getMessage(); 
+        }
+        return $error;
+    }
+
+
+    public function removeDir($ftpDir)
+    {
+        if($ftpDir === '\\' || $ftpDir === '/') 
+        { 
+            echo("Cannot delete root directory"); 
+            return false; 
+        }
+        if (isset($this->hConnection))
+        {  
+            if($this->isFileExist($ftpDir))
+            {
+                $this->deleteDirAndFiles($ftpDir);
+            }
+        }
+    }
+
+    // public function uploadDir($localDir, $ftpDir)
+    // {
+    //     if ($this->hConnection) 
+    //     {  
+    //         if($this->isFileExist($ftpDir)) // ?? parent maybe
+    //         {
+    //             $this->uploadDirAndFiles($localDir, $ftpDir);
+    //         }
+    //     }
+
+    // }
+
+
+    private function deleteDirAndFiles($fullPathDir)
     {
         if(isValidDir($fullPathDir))
         {   
-            if (\ftp_chdir($ftpConn, $fullPathDir)) 
+            if (\ftp_chdir($this->hConnection, $fullPathDir)) 
             {
-                $curDirRestore = \ftp_pwd($ftpConn); // store here, otherwise error will occur during deleting folders and files, because current dir changes
+                $curDirRestore = \ftp_pwd($this->hConnection); // store here, otherwise error will occur during deleting folders and files, because current dir changes
 
-                //-A parameter is not supported by all ftp apps
-                $rawPaths = \ftp_rawlist($ftpConn, "-A .");
-                $filePaths = \ftp_nlist($ftpConn, "-A .");
+                //-A is not supported by all ftp apps
+                $rawPaths = \ftp_rawlist($this->hConnection, "-A .");
+                $filePaths = \ftp_nlist($this->hConnection, "-A .");
             
                 if(\count($rawPaths) == \count($filePaths))
                 {
@@ -54,17 +158,16 @@ class FtpHandler
                     {
                         if(isStrEndsWith($rawPaths[$i], $filePaths[$i]))
                         {
-                            // is dir
                             if(isValidDir($filePaths[$i]))
                             {
                                 if($rawPaths[$i][0] === 'd')
                                 {
-                                    $this->deleteDirAndFiles($ftpConn, $curDirRestore.'/'.$filePaths[$i]);
+                                    $this->deleteDirAndFiles($curDirRestore.'/'.$filePaths[$i]);
                                 }
                                 else
                                 {
-                                    //$isfilechmo = ftp_chmod($ftpConn, 0777, $filePaths[$i]); // seems like doesn't work remotely, only locally??
-                                    if(!\ftp_delete($ftpConn, $curDirRestore.'/'.$filePaths[$i]))
+                                    //$isfilechmo = ftp_chmod($this->hConnection, 0777, $filePaths[$i]); // doesn't work? check again with pasv=true
+                                    if(!\ftp_delete($this->hConnection, $curDirRestore.'/'.$filePaths[$i]))
                                     {
                                         echo "failed to delete file ".$filePaths[$i].PHP_EOL;
                                     }
@@ -73,7 +176,7 @@ class FtpHandler
                         }
                     }
 
-                    if(!\ftp_rmdir($ftpConn , $fullPathDir))
+                    if(!\ftp_rmdir($this->hConnection , $fullPathDir))
                     {
                         echo "failed to delete folder ".$fullPathDir.PHP_EOL;
                     }
@@ -88,33 +191,11 @@ class FtpHandler
     }
 
 
-    public function removeDir($ftpServer, $ftpUser, $ftpPass, $ftpDir)
-    {
-        if(\strlen($ftpDir) < 2) { die("Cannot delete root directory"); }
-        $ftpConn = \ftp_connect($ftpServer) or die("Error: ftp: $ftpServer: Connection has failed!");
-        $login = \ftp_login($ftpConn, $ftpUser, $ftpPass);
-        if ($login) 
-        {  
-            //nlist, rawlist, get, put do not work in active mode
-            \ftp_pasv($ftpConn, true);
-            
-            if($this->isFileExist($ftpConn, $ftpDir))
-            {
-                $this->deleteDirAndFiles($ftpConn, $ftpDir);
-            }
-            else
-            {
-                echo "Error: ftp: $ftpDir: File doesn't exist";
-            }
-        }
-        else
-        {
-            echo ("Error: ftp: $ftpUser: Login name or password is incorrect!");
-        }
 
-        // close connection
-        \ftp_close($ftpConn);
-    }
+
+
+
+
 
 };
 
